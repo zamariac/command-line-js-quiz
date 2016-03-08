5ee0a7c4d68b908fceb4ecafa7d807ffa161b042
Maria Zamora <Zamora.Mariac@gmail.com>
2016-03-08 17:07:02 -0600

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+var questionOne = prompt('what does css stand for?');
+var answerOne = 'cascading style sheet';
+var answerOneEmpty = " ";
+
+//
+if (questionOne === answerOne){
+	console.log ("You are correct!")
+}else if (questionOne === answerOneEmpty){
+	console.log ("Please enter an answer!")
+}else{
+	(questionOne !== answerOne)
+		console.log("You're wrong!")
+}
+
+
+
+// if (condition) {
+//    statements1
+// } else {
+//    statements2
+// }
