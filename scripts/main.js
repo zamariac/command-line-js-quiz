ed0dd6a1f71ce8b6a746e8cbef62d9fe81ee78b7
Maria Zamora <Zamora.Mariac@gmail.com>
2016-03-08 17:27:58 -0600

    initial questions

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,38 @@
 var questionOne = prompt('what does css stand for?');
 var answerOne = 'cascading style sheet';
-var answerOneEmpty = " ";
 
-//
 if (questionOne === answerOne){
 	console.log ("You are correct!")
-}else if (questionOne === answerOneEmpty){
-	console.log ("Please enter an answer!")
-}else{
+}else if (questionOne === ''){
+	console.log ("Please enter an answer");
+}else {
 	(questionOne !== answerOne)
-		console.log("You're wrong!")
-}
+		console.log("You are wrong!")
+	}
 
+var questionTwo = prompt('what kind of element is a <div>?');
+var answerTwo = 'block';
+
+if (questionTwo === answerTwo){
+	console.log ("You are correct!")
+}else if (questionTwo === ''){
+	console.log ("Please enter an answer");
+}else {
+	(questionTwo !== answerTwo)
+		console.log("You are wrong!")
+	}
+
+var questionThree = prompt('How do you count string characters?');
+var answerThree = '.length';
+
+if (questionThree === answerThree){
+	console.log ("You are correct!")
+}else if (questionThree === ''){
+	console.log ("Please enter an answer");
+}else {
+	(questionThree !== answerThree)
+		console.log("You are wrong!")
+	}
 
 
 // if (condition) {
